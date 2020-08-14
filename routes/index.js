aacf9793d668311a8279ca1824f8728f7d106297
Tan Tano <tantn1@sunshinegroup.vn>
2020-08-14 11:23:45 +0700

    connect postgreeSql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'tano-api',
+    password: 'tano',
+    port: 5432,
+});
+
+
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  client.connect()
+  client.query('SELECT * from contact')
+  .then(result => res.json(result.rows))
+  .catch(error => console.log(error));
 });
 
 module.exports = router;
