58f78ef960836eea90849c91139d23bf7cd3a283
Tan Tano <tantn1@sunshinegroup.vn>
2020-08-21 14:59:02 +0700

    manage-tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -15,3 +15,75 @@ const tasks = [
     {id: 4, title: 'Go to sleep', description : 'I something go to bed at 11 pm', status: 4},
 ];
 
+
+router.get('/', function (req, res, next) {
+    const tasksLocalStorage = JSON.parse(localStorage.getItem('node-tasks'));
+    if (tasksLocalStorage) {
+        res.json(tasksLocalStorage);
+    } else {
+        localStorage.setItem('node-tasks', JSON.stringify(tasks));
+        res.json(tasks);
+    }
+});
+
+router.get('/:id', function (req, res, next) {
+    const tasksLocalStorage = JSON.parse(localStorage.getItem('node-tasks'));
+    const id = req.params.id;
+    if (tasksLocalStorage) {
+        const taskIndex = tasksLocalStorage.findIndex(task => task.id == id);
+        if (taskIndex !== -1) {
+            res.json(tasksLocalStorage[taskIndex]);
+        } else {
+            res.json(null);
+        }
+    } else {
+        res.json(null);
+    }
+});
+
+
+router.post('/add', function (req, res, next) {
+    const tasksLocalStorage = JSON.parse(localStorage.getItem('node-tasks'));
+    const data = req.body;
+    if (tasksLocalStorage.length == 0) {
+        data.id = 1;
+    } else {
+        const id = Math.max(...tasksLocalStorage.map(t=> +t.id)) + 1;
+        data.id = id;
+    }
+    tasksLocalStorage.push(data);
+    localStorage.setItem('node-tasks', JSON.stringify(tasksLocalStorage));
+    res.json(data);
+});
+
+router.delete('/delete/:id', function (req, res, next) {
+    const tasksLocalStorage = JSON.parse(localStorage.getItem('node-tasks'));
+    const id = req.params.id;
+    if (tasksLocalStorage && tasksLocalStorage.length > 0) {
+        const index = tasksLocalStorage.findIndex(task => task.id == id);
+        if (index !== -1) {
+            tasksLocalStorage.splice(index, 1);
+            localStorage.setItem('node-tasks', JSON.stringify(tasksLocalStorage));
+        }
+    }
+    res.json(tasksLocalStorage);
+});
+
+
+router.post('/edit', function (req, res, next) {
+    const tasksLocalStorage = JSON.parse(localStorage.getItem('node-tasks'));
+    const taskUpdate = req.body;
+    if (tasksLocalStorage && tasksLocalStorage.length > 0) {
+        const task = tasksLocalStorage.find(task => task.id == taskUpdate.id);
+        if (task) {
+            task.title = taskUpdate.title;
+            task.description = taskUpdate.description;
+            task.status = taskUpdate.url;
+            localStorage.setItem('node-tasks', JSON.stringify(tasksLocalStorage));
+        }
+    }
+    localStorage.setItem('node-tasks', JSON.stringify(tasksLocalStorage));
+    res.json(tasksLocalStorage);
+});
+
+module.exports = router;
